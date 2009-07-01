eb46ae5c58f4d6846d2940eefe2b29b48d591334
rohit.majhi@gmail.com <rohit.majhi@gmail.com@df79f6dc-56ab-11de-90f7-d36b043dce00>
2009-07-01 21:53:33 +0000

    committing R.java

--- DueDate/trunk/gen/com/motleymusketeers/duedate/R.java
+++ DueDate/trunk/gen/com/motleymusketeers/duedate/R.java
@@ -11,18 +11,21 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int icon_add=0x7f020000;
+        public static final int icon_sub=0x7f020001;
+        public static final int icon_timesheet=0x7f020002;
     }
     public static final class id {
         public static final int AbsoluteLayout=0x7f050000;
+        public static final int Button_Decrement=0x7f050003;
+        public static final int Button_Increment=0x7f050005;
         public static final int StartDate=0x7f050002;
-        public static final int TxtBox_WorkingDays=0x7f050003;
-        public static final int TxtView_Result=0x7f050004;
+        public static final int TxtBox_WorkingDays=0x7f050004;
+        public static final int TxtView_Result=0x7f050006;
         public static final int TxtView_Start=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int main1=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
